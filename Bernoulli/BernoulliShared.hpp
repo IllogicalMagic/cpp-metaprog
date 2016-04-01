c203f75128ad5c57fdfcbcc1560bbce0e084557b
IllogicalMagic <coillol@yandex.ru>
2016-04-01 12:14:58 +0300

    Some template code moved to BernoulliShared.hpp.

---
+++ Bernoulli/BernoulliShared.hpp
@@ -0,0 +1,97 @@
+#ifndef BERNOULLI_SHARED_HPP_INCLUDED_
+#define BERNOULLI_SHARED_HPP_INCLUDED_
+
+#include <cstdint>
+#include <ratio>
+#include <array>
+
+#include "DynamicRatio.hpp"
+
+#ifndef B_SIZE
+#define B_SIZE 3ul
+#endif
+
+using bern = std::uint64_t;
+
+// Binomial quotient C(n,k)
+template<bern N, bern K>
+struct Binomial
+{
+  using value=
+    std::ratio_add<typename Binomial<N-1,K-1>::value,
+		   typename Binomial<N-1,K>::value>;
+};
+
+template<bern N>
+struct Binomial<N,0>{using value=std::ratio<1>;};
+
+template<bern N>
+struct Binomial<N,N>{using value=std::ratio<1>;};
+
+// Struct for computing sum for Bernoulli number
+// Should be implemented in modules
+template<bern N>
+struct BSum;
+
+// Skip odd numbers
+template<bern N,bool Opt>
+struct BernFast;
+
+template<bern N>
+struct BernFast<N,true>
+{
+  using value=std::ratio<0>;
+};
+
+template<bern N>
+struct BernFast<N,false>
+{
+  using value=
+    std::ratio_multiply<std::ratio<-1,N+1>,typename BSum<N>::value>;
+};
+
+// Main struct for numbers
+template<bern N>
+struct Bernoulli
+{
+  using value=typename BernFast<N,N&1>::value;
+};
+
+// Predefined values for special numbers
+template<>
+struct Bernoulli<1>
+{
+  using value = std::ratio<-1,2>;
+};
+
+template<>
+struct Bernoulli<0>
+{
+  using value = std::ratio<1>;
+};
+
+// Struct to dynamically fill std::array
+template<bern N>
+struct BernoulliList
+{
+  template<size_t Size>
+  static void fill(std::array<DRatio::DRatio,Size>& a)
+  {
+    a[N].num=Bernoulli<N>::value::num;
+    a[N].den=Bernoulli<N>::value::den;
+    BernoulliList<N-1>::fill(a);
+  }
+};
+
+template<>
+struct BernoulliList<0>
+{
+  template<size_t Size>
+  static void fill(std::array<DRatio::DRatio,Size>& a)
+  {
+    a[0].num=Bernoulli<0>::value::num;
+    a[0].den=Bernoulli<0>::value::den;
+  }
+};
+
+#endif
