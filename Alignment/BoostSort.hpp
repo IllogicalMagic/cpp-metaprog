7174c5bcbd28c245ff721df1217e3751c78ac24e
IllogicalMagic <coillol@yandex.ru>
2016-04-25 21:30:11 +0300

    BoostSort.hpp: added index tracking for tuple elements.

--- Alignment/BoostSort.hpp
+++ Alignment/BoostSort.hpp
@@ -11,6 +11,29 @@
 #include <utility>
 #include <tuple>
 
+// Pair (Elem,Index)
+template<typename T, size_t I>
+struct TupleElem
+{
+  using type = T;
+  static constexpr size_t index = I;
+};
+
+// Form new predicate for TupleElem<T,I> from predicate for T
+template<template<typename T1, typename T2> class Pred>
+struct GetIndexPred
+{
+  template<typename T1, typename T2>
+  struct IndexPred
+  {
+    using type = 
+      typename Pred<typename T1::type,typename T2::type>::type;
+  };
+
+  template<typename T1,typename T2>
+  using type = IndexPred<T1,T2>;
+};
+
 template<template <typename T1, typename  T2> class Pred,
 	 typename Tpl>
 struct TupleSortImpl
@@ -18,22 +41,34 @@ struct TupleSortImpl
   using _1 = boost::mpl::_1;
   using _2 = boost::mpl::_2;
 
-  template<typename... Args>
-  static auto tuple_to_vector(std::tuple<Args...>)
-  -> boost::mpl::vector<Args...>;
+  // Get new Pred
+  template<typename T1,typename T2>
+  using IndexPred = 
+    typename GetIndexPred<Pred>::template type<T1,T2>;
 
-  using vec_types = decltype(tuple_to_vector( std::declval<Tpl>() ));
-  using sorted = typename boost::mpl::sort<vec_types,Pred<_1,_2>>::type;
+  static constexpr size_t size = std::tuple_size<Tpl>::value;
+  using index_seq = std::make_index_sequence<size>;
+
+  // Form TupleElem vector
+  template<typename... Args,size_t...S>
+  static auto tuple_to_vector
+  (std::tuple<Args...>,std::index_sequence<S...>)
+    -> boost::mpl::vector<TupleElem<Args,S>...>;
+
+  using vec_types = 
+    decltype(tuple_to_vector( std::declval<Tpl>(),
+			      index_seq() ));
+  using sorted = 
+    typename boost::mpl::sort<vec_types,IndexPred<_1,_2>>::type;
 
   template <typename Vec, size_t...S>
-  static auto cont_to_tuple(Vec,std::index_sequence<S...>) 
-  -> std::tuple<typename boost::mpl::at_c<Vec,S>::type...>;
-
-  using size = typename boost::mpl::size<vec_types>::type;
+  static auto cont_to_tuple(Vec,std::index_sequence<S...>)
+  // decltype(at_c<Vec,S>::type)==TupleElem
+    -> std::tuple<typename boost::mpl::at_c<Vec,S>::type::type...>;
 
   using type = 
     decltype(cont_to_tuple(std::declval<sorted>(),
-		       std::make_index_sequence<size::value>() ));
+			   index_seq() ));
 };
 
 #endif
